77e651a92f1394c185844300331d9fb38467af2f
Naresh <naresh@Nareshs-MacBook-Air.local>
2025-06-15 20:25:44 -0500

    change view

--- components/PostFeed.tsx
+++ components/PostFeed.tsx
@@ -37,7 +37,7 @@ export default function PostFeed() {
         className="py-4 px-3 text-lg sm:text-xl sticky top-0 
          z-50 bg-white bg-opacity-80 backdrop-blur-sm font-bold
          border-b border-gray-100">
-         share your thoughts freely
+         share your views
       </div>
     <PostInput/>
     {posts.map(post => <Post
